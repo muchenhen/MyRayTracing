c7cd9a17c948e4dc30d5a75955bd571574ce5f69
muchenhen <44308876+muchenhen@users.noreply.github.com>
2020-09-09 20:22:16 +0800

    1.10
    第一个渐变图像，颜色是距离光线原点的距离，沿光线方向越近越亮

--- MyRayTracing/OneWeekend/OneWeekend.cpp
+++ MyRayTracing/OneWeekend/OneWeekend.cpp
@@ -25,6 +25,16 @@ int main()
 		cout << "open file failed" << endl;
 	}
 
+	//Camera
+	auto viewport_height = 2.0;
+	auto viewport_width = aspect_ratio * viewport_height;
+	auto foacl_length = 1.0;//the distance between the projection plane and the projection point 
+
+	auto origin = point3(0, 0, 0);
+	auto horizontal = vec3(viewport_width, 0, 0);
+	auto vertical = vec3(0, viewport_height, 0);
+	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, foacl_length);
+
 	// Render
 	outfile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 	for (int j = image_height - 1; j >= 0; --j) 
@@ -33,10 +43,10 @@ int main()
 		for (int i = 0; i < image_width; ++i) 
 		{
 			Eigen::Vector3f pixel_color;
-			float r = float(i) / (float(image_width) - 1);
-			float g = float(j) / (float(image_height) - 1);
-			float b = 0.25;
-			pixel_color = { r,g,b };
+			auto u = double(i) / (image_width - 1);
+			auto v = double(j) / (image_height - 1);
+			Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			pixel_color = color::ray_color(r);
 			color::write_color(outfile, pixel_color);
 		}
 	}
