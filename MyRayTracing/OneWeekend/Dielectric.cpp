1972e26fcf9660d9f6e6bc0780d297112678389b
muchenhen <44308876+muchenhen@users.noreply.github.com>
2020-09-26 17:12:34 +0800

    5.10
    完全内部的反射

--- MyRayTracing/OneWeekend/Dielectric.cpp
+++ MyRayTracing/OneWeekend/Dielectric.cpp
@@ -10,6 +10,17 @@ bool Dielectric::Scatter(const Ray& r_in, const hitRecord& rec, color3& attenuat
 	attenuation = color3(1.0, 1.0, 1.0);
 	double etaiOverEtat = rec.frontFace ? (1.0 / refIndex) : refIndex;
 	vec3 unitDirection = Vec3Unit(r_in.dir);
+
+	//全内反射 没有折射
+	double cosTheta = fmin(-unitDirection.dot(rec.normal), 1.0);
+	double sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+	if (etaiOverEtat * sinTheta > 1.0)
+	{
+		vec3 refracted = Reflect(unitDirection, rec.normal);
+		scattered = Ray(rec.p, refracted);
+		return true;
+	}
+
 	vec3 refracted = Refract(unitDirection, rec.normal, etaiOverEtat);
 	scattered = Ray(rec.p, refracted);
 	return true;
--- MyRayTracing/OneWeekend/OneWeekend.cpp
+++ MyRayTracing/OneWeekend/OneWeekend.cpp
@@ -46,9 +46,9 @@ int main()
 
 	//Materials
 	auto materialGround = make_shared<Lambertian>(color3(0.8, 0.8, 0.0));
-	auto materialCenter = make_shared<Dielectric>(1.5);
+	auto materialCenter = make_shared<Lambertian>(color3(0.1, 0.2, 0.5));
 	auto materialLeft = make_shared<Dielectric>(1.5);
-	auto materialRight = make_shared<Metal>(color3(0.8, 0.6, 0.2),1.0);
+	auto materialRight = make_shared<Metal>(color3(0.8, 0.6, 0.2),0.0);
 
 	world.add(make_shared<Sphere>(point3( 0.0, -100.5, -1.0), 100.0, materialGround));
 	world.add(make_shared<Sphere>(point3( 0.0,	  0.0, -1.0),   0.5, materialCenter));
